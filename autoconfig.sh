6f3d9fdeca578ba7a8504f142bda921ac0acc1f8
fangxuehouwuming <2416811040@qq.com>
2024-02-29 16:21:54 +0800

    Add running bash

--- autoconfig.sh
+++ autoconfig.sh
@@ -4,3 +4,5 @@ files=$(ls -a $1 | grep -E '.[^.]+' |grep -v .git)
 for file in `echo $files`; do
 	ln -s $1/$file ~/$file # 创建软链接
 done
+# ~ $ source autoconfig.sh 
+# 执行脚本，为dotfiles中的配置文件创建在主目录 ~ 下的软链接
